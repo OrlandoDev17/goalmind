b9db8d75934195efb7ad8099fae9360b179b81c8
OrlandoDev17 <orlandodev1707@gmail.com>
2025-05-16 01:31:58 -0400

    Agregar componente Welcome: implementar una sección de bienvenida con botones de navegación

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,28 @@
+import { RightArrowIcon } from './Icons';
+import Button from './Button';
+
+export default function Welcome() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-6 ">
+      <h1 className="text-6xl font-bold text-blue-500 z-10">
+        Goal<span className="text-black">Mind</span>
+      </h1>
+      <p className="text-2xl text-gray-600 max-w-200 text-center">
+        Visualiza, planifica y alcanza tus objetivos con el poder de la
+        inteligencia artificial
+      </p>
+      <div className="flex gap-4 z-10">
+        <Button href="/dashboard">
+          Comenzar Ahora
+          <RightArrowIcon className="size-8" />
+        </Button>
+        <a
+          className="bg-white px-6 py-3 rounded-lg border-1 border-gray-200 text-lg font-semibold shadow-lg hover:bg-green-200 hover:-translate-y-1 transition"
+          href="#features"
+        >
+          Descubrir Más
+        </a>
+      </div>
+    </div>
+  );
+}
