a6b0a2ec02b83f072996d9f49c2be8cbb9e94677
OrlandoDev17 <orlandodev1707@gmail.com>
2025-05-16 01:31:30 -0400

    Agregar componente FeatureCards: implementar un nuevo componente para mostrar información de características

---
+++ src/components/FeatureCards.tsx
@@ -0,0 +1,22 @@
+import { FEATURES_INFO } from '@/consts/FeaturesInfo';
+
+export default function FeatureCards() {
+  return (
+    <>
+      <div className="grid grid-cols-1 2xl:grid-cols-3 justify-center items-center gap-4">
+        {FEATURES_INFO.map(({ id, icon: Icon, title, description }) => (
+          <article
+            className="flex justify-center items-center bg-white p-4 w-256 2xl:w-110 min-h-56 rounded-lg shadow-lg border-1 border-gray-200"
+            key={id}
+          >
+            <div className="flex flex-col gap-4">
+              <Icon className="size-12 text-blue-500" />
+              <h3 className="text-2xl font-bold">{title}</h3>
+              <p className="2xl:w-100 text-lg text-gray-600 ">{description}</p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </>
+  );
+}
